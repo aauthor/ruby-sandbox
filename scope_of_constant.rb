9d62dbe75f411c453f326e3558ff4457ace6a56d
Tom Dunlap <apocryphalauthor@gmail.com>
2014-11-20 14:52:25 -0500

    Add scope_of_constant.rb

---
+++ scope_of_constant.rb
@@ -0,0 +1,17 @@
+require "active_support/core_ext"
+require "pry-byebug"
+
+class Foo
+  CON = :foo
+  def pcon; p CON; end
+end
+
+class Bar < Foo
+  CON = :bar
+  def pcon
+    super
+    p CON
+  end
+end
+
+Bar.new.pcon
